9409994f69a993bdb794f7def2eeabea9b295b08
macraves <macius.caner@gmail.com>
2023-10-22 00:12:59 +0100

    app object created

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 """Backend for the web app."""
 import flask
 
-app = flask.Flask(__name__)
+
+class ApiErrors(Exception):
+    """Custom exceptions to point out errors in the API."""
+
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
 
 
-@app.route("/", methods=["GET"])
-def index():
-    return "Hello, world!"
+class AppApi:
+    def __init__(self, user):
+        self.app = flask.Flask(__name__)
+        self.user = user
+
+    def index(self):
+        return f"Hello, {self.user}!"
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def test_api():
+    var = AppApi("caner")
+    var.app.add_url_rule("/", view_func=var.index, methods=["GET"])
+    var.app.run(debug=True)
